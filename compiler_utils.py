3069d41cecb32726e0f0db960e245c34b58f1d96
Philippe Guyard <philippeg@DESKTOP-G91EOHE>
2024-03-05 16:47:09 +0000

    Initial commit

--- compiler_utils.py
+++ compiler_utils.py
@@ -30,41 +30,10 @@ def try_compile_cpp(src_path: Path=None, lines: List[str]=None, text: str=None):
     if src_path is None:
         src_path = ensure_file(lines, text)
     
-    cmd = ["g++", "-c", '-Wall', '-fdiagnostics-format=json', src_path.as_posix()]
-    out = subprocess.run(cmd, capture_output=True, text=True)
-    print(out)
+    cmd = ["g++", '-Wall', '-fdiagnostics-format=json', src_path.as_posix()]
+    out = subprocess.run(cmd, capture_output=True)
     return ParsedCompilerOutput(
         returncode=out.returncode,
         stdout=out.stdout,
-        stderr=None # TODO: This breaks
+        stderr=json.loads(out.stderr)
     )
-
-# def try_compile_cpp_alt(src_path: Path=None, lines: List[str]=None, text: str=None): 
-#     """Attempts to compile a C++ program contained in src_path
-
-#     Args:
-#         src_path (Path, optional): Path to .txt file with the program. Defaults to None.
-#         lines (List[str], optional): _description_. Defaults to None.
-#         text (str, optional): _description_. Defaults to None.
-
-#     Returns:
-#         boolean: True if compilation successful, False otherwise
-#     """
-#     compiler = "g++" 
-#     command = [compiler, "-c", '-Wall', '-fdiagnostics-format=json', src_path.as_posix()]
-
-#     try:
-#         result = subprocess.run(command, capture_output=True, text=True)
-#         if result.returncode == 0:
-#             print("Compilation successful!")
-#             return True
-#         else:
-#             print("Compilation failed:")
-#             print(result.stderr)  
-#             return False
-
-#     except FileNotFoundError:
-#         print(f"Error: C++ file not found at '{src_path}'")
-#         return False
-test = try_compile_cpp(src_path=Path("/cs/student/projects3/COMP0087/grp2/clean/exp-codellama-7b/evals/24/code_output.txt"))
-print(test)
