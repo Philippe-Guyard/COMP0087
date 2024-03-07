fafa14af2593b70e382f97c5e96d57ab0e599e5b
Philippe GUYARD <pguyard@mandarin.cs.ucl.ac.uk>
2024-03-07 14:20:30 +0000

    Modify prompt and fix code finding from output

--- prompt_utils.py
+++ prompt_utils.py
@@ -14,33 +14,33 @@ def find_nth(haystack: str, needle: str, n: int) -> int:
     return start
 
 def parse_code(output: str, n_examples=1):
-    code_start_idx = find_nth(output, 'Output:', n_examples + 1) + len('Output:') + 1                                                     
-    # The last } probably signifies the end of the main function
-    # We truncate the output up to it in case the model left some notes explaining the code 
-    code_end_idx = output.rfind('}') 
+    # NOTE: Assume the code is between the 8th and 9th ``` (1 for sys prompts, 4 for the example, 2 for the input)
+    code_delimiter = '#####'
+    code_start_idx = find_nth(output, code_delimiter, 8) + len(code_delimiter)
+    code_end_idx   = find_nth(output, code_delimiter, 9) 
 
-    return output[code_start_idx : code_end_idx + 1]
+    return output[code_start_idx : code_end_idx]
 
 def make_prompt_template(truncated_txt: str):
     return [
     {
         'role': 'user',
         'content': '''You are an assistant that helps users with writing compiler-friendly C++ programmes. Your outputs should be exclusively C++ programmes that can be compiled with C++17.
-Please make sure to delimit your code with ```. Here is an example:
-```
+Please make sure to delimit your code with #####. Here is an example:
+#####
 #include <iostream>
 
 using namespace std; 
 
 int main() {{
     cout << "Hello, 
-```
+#####
 '''
     },
     {
         'role': 'assistant',
         'content': '''
-```
+#####
 #include <iostream>
 
 using namespace std; 
@@ -48,11 +48,12 @@ using namespace std;
 int main() {{
     cout << "Hello, World! << endl;
 }}
-```'''
+#####
+'''
     },
     {
         'role': 'user',
-        'content': '```\n' + truncated_txt + '\n```'
+        'content': '#####\n' + truncated_txt + '\n#####\n'
     },
     ]
 
@@ -61,6 +62,6 @@ def make_sft_example(txt: str):
     prompt_template = make_prompt_template(truncated_txt)
     prompt_template.append({
         'role': 'assistant',
-        'content': '```\n' + txt + '\n```'  
+        'content': '#####\n' + txt + '\n#####'  
     })
     return prompt_template
