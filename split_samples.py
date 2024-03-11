aea21da66d6c28ae91790c5c7d4905285c8556a1
Philippe GUYARD <pguyard@mandarin.cs.ucl.ac.uk>
2024-03-11 19:35:35 +0000

    Fix split_samples

--- split_samples.py
+++ split_samples.py
@@ -1,28 +1,27 @@
 import json 
 
-from experiments import Experiment
+from experiments import Experiment, EXPERIMENTS_ROOT
 
-experiment = Experiment(
-    exp_name='tmp',
-    exp_type='eval',
-    seq_length=0 # anything 
-)
-
-samples_file_path = './samples.txt'
+exp_name = 'eval_mistral_sft'
+exp_root = EXPERIMENTS_ROOT.joinpath(exp_name)
+samples_file_path= './samples_big.txt'
 samples = []
 with open(samples_file_path) as samples_file:
     samples = samples_file.readlines() 
 
 results = None 
-with open(experiment.root_folder.joinpath('results.json'), 'w') as results_file:
-    results_file = json.load(results_file)
+with open(exp_root.joinpath('results.json')) as results_file:
+    results = json.load(results_file)
 
 sample_results = []
 good_samples, bad_samples = 0, 0
-for seq_id, seq_result in results_file.items():
-    status = seq_result['status']
-    sample_path = samples[seq_id]
-    output_path = experiment.root_folder.joinpath(f'evals/{seq_id}/output.txt')
+for seq_id, seq_result in results.items():
+    if 'Status' not in seq_result:
+        print('Invalid seq result:', seq_result.keys())
+        continue
+    status = seq_result['Status']
+    sample_path = samples[int(seq_id)]
+    output_path = exp_root.joinpath(f'evals/{seq_id}/output.txt')
     assert status in ('Good', 'Bad'), f'Invalid status: {status}'
 
     good_samples += status == 'Good'
@@ -30,7 +29,7 @@ for seq_id, seq_result in results_file.items():
     sample_results.append({
         'status': status,
         'sample_path': sample_path,
-        'output_path': output_path 
+        'output_path': output_path.as_posix() 
     })
 
 print('Percentage of good samples: ', good_samples / (good_samples + bad_samples))
