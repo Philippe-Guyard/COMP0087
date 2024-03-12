1e0771fc099483c67fbfa9a1082dd1b7875e139c
Philippe Guyard <philippe.guyard@outlook.com>
2024-03-12 13:27:58 +0100

    Select both train and test samples in select_samples.py

--- select_samples.py
+++ select_samples.py
@@ -11,23 +11,38 @@ from codenet_data import CodeNetIter, CodeNetSolution
 from codenet_utils import CachedMetadataReader
 from compiler_utils import try_compile_cpp
 
+TRAIN_PATH = './samples/samples_train.txt'
+TEST_PATH = './samples/samples_test.txt'
+
 metadata_cache = CachedMetadataReader(maxsize=100)
 
 myiter = CodeNetIter(language='C++', limit=None)
-pids_seen = defaultdict(int)
-MAX_PIDS = 5000
-MAX_PBS_PER_PID = 5
+# train, test 
+pids_seen = (defaultdict(int), defaultdict(int))
+TRAIN_PBS_PER_PID = 5
+TEST_PBS_PER_PID = 2
+dataset = [
+    [], # train 
+    []  # test 
+]
 for x in myiter:
     x: CodeNetSolution
     x_meta = metadata_cache.read_meta(x.metadata_path, x.submission_id)
     accepted = x_meta is not None and x_meta.status == 'Accepted'
-    if accepted and pids_seen[x.problem_id] < MAX_PBS_PER_PID:
-        out = try_compile_cpp(src_path=x.src_path)
-        if out.returncode != 0:
-            continue
+    if not accepted:
+        continue
+    if pids_seen[x.problem_id][0] >= TRAIN_PBS_PER_PID and pids_seen[x.problem_id][1] >= TEST_PBS_PER_PID:
+        continue
 
-        pids_seen[x.problem_id] += 1
-        print(x.src_path)
+    out = try_compile_cpp(src_path=x.src_path)
+    if out.returncode != 0:
+        continue
+    
+    dataset_idx = 0 if pids_seen[x.problem_id][0] >= TRAIN_PBS_PER_PID else 1 
+    pids_seen[x.problem_id][dataset_idx] += 1
 
-    if len(pids_seen) > MAX_PIDS:
-        break
+with open(TRAIN_PATH, 'w') as train_file:
+    train_file.writelines(dataset[0])
+
+with open(TEST_PATH, 'w') as test_file:
+    test_file.writelines(dataset[1])
