6cf024e01e155ed9cddc3f11089bec22c767626c
Philippe GUYARD <pguyard@mallard.cs.ucl.ac.uk>
2024-03-15 11:55:04 +0000

    Fix a bug in select_samples

--- select_samples.py
+++ select_samples.py
@@ -12,19 +12,20 @@ from codenet_utils import CachedMetadataReader
 from compiler_utils import try_compile_cpp
 from experiments import NLPDataset
 
-DATASET_NAME = 'samples_big'
+DATASET_NAME = 'big_no_limit'
 dataset = NLPDataset('samples', DATASET_NAME)
 metadata_cache = CachedMetadataReader(maxsize=100)
 
 myiter = CodeNetIter(language='C++', limit=None)
 # train, test 
-pids_seen = (defaultdict(int), defaultdict(int))
+pids_seen = defaultdict(lambda: list((0, 0)))
 TRAIN_PBS_PER_PID = 5
 TEST_PBS_PER_PID = 2
 dataset_values = [
     [], # train 
     []  # test 
 ]
+cnt = 0
 for x in myiter:
     x: CodeNetSolution
     x_meta = metadata_cache.read_meta(x.metadata_path, x.submission_id)
@@ -38,12 +39,16 @@ for x in myiter:
     if out.returncode != 0:
         continue
     
-    dataset_idx = 0 if pids_seen[x.problem_id][0] >= TRAIN_PBS_PER_PID else 1 
+    cnt += 1
+    if cnt % 100 == 0:
+        print(cnt)
+
+    dataset_idx = 1 if pids_seen[x.problem_id][0] >= TRAIN_PBS_PER_PID else 0 
     pids_seen[x.problem_id][dataset_idx] += 1
-    dataset_values[dataset_idx].append(x.src_path)
+    dataset_values[dataset_idx].append(x.src_path.as_posix())
 
 with open(dataset.get_path('train'), 'w') as train_file:
-    train_file.writelines(dataset_values[0])
+    train_file.write('\n'.join(dataset_values[0]))
 
 with open(dataset.get_path('test'), 'w') as test_file:
-    test_file.writelines(dataset_values[1])
+    test_file.writelines('\n'.join(dataset_values[1]))
